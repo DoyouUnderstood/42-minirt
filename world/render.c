380c638b08300fd37a6b054737b9497de9eadf0c
Alex Letondor <alletond@c1r6s4.42lausanne.ch>
2024-05-15 17:59:48 +0200

    norme

--- world/render.c
+++ world/render.c
@@ -33,22 +33,6 @@ void setup_thread_data(t_renderthread *thread_data, t_world *world, t_matrix inv
     thread_data->end_y = (i == num_threads - 1) ? world->camera->vsize : (i + 1) * segment_height;
 }
 
-void initialize_and_launch_threads(RenderSetup *setup, t_world *world) 
-{
-    setup->inverse_transform = matrix_inverse(world->camera->transform);
-    setup->segment_height = world->camera->vsize / setup->num_threads;
-
-    for (int i = 0; i < setup->num_threads; i++) {
-        setup_thread_data(&setup->thread_data[i], world, setup->inverse_transform, i, setup->num_threads, setup->segment_height);
-        pthread_create(&setup->threads[i], NULL, render_section, &setup->thread_data[i]);
-    }
-
-    for (int i = 0; i < setup->num_threads; i++) {
-        pthread_join(setup->threads[i], NULL);
-    }
-}
-
-
 // render fonction avec remaining pour nombre de reflect a calculer
 void	*render_section(void *arg)
 {
@@ -74,16 +58,31 @@ void	*render_section(void *arg)
 	return (NULL);
 }
 
+void initialize_and_launch_threads(RenderSetup *setup, t_world *world) 
+{
+    setup->inverse_transform = matrix_inverse(world->camera->transform);
+    setup->segment_height = world->camera->vsize / setup->num_threads;
+
+    for (int i = 0; i < setup->num_threads; i++) {
+        setup_thread_data(&setup->thread_data[i], world, setup->inverse_transform, i, setup->num_threads, setup->segment_height);
+        pthread_create(&setup->threads[i], NULL, render_section, &setup->thread_data[i]);
+    }
+
+    for (int i = 0; i < setup->num_threads; i++) {
+        pthread_join(setup->threads[i], NULL);
+    }
+}
+
 void render(t_world *world) {
     RenderSetup setup;
-    setup.num_threads = 15; // Number of threads can be fixed or determined based on hardware capabilities
+    setup.num_threads = 15;
     setup.threads = malloc(setup.num_threads * sizeof(pthread_t));
     setup.thread_data = malloc(setup.num_threads * sizeof(t_renderthread));
 
     if (!setup.threads || !setup.thread_data) {
-        free(setup.threads);   // Ensure proper resource management
+        free(setup.threads);
         free(setup.thread_data);
-        return; // Error handling
+        return; 
     }
 
     initialize_and_launch_threads(&setup, world);
