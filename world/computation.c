ecbb89253933ff6b220821c889058104ef6fdcc0
ltd <Alex.letondor@outlook.fr>
2024-05-15 14:35:29 +0000

    norme

--- world/computation.c
+++ world/computation.c
@@ -1,83 +1,122 @@
 #include "../include/main.h"
-
 #include "../include/mathematique.h"
 #include "../include/shape.h"
-#include "../object/test_shape.h"
 #include "../lib/libft/libft.h"
+#include "../object/test_shape.h"
 #define EPSILON 0.00001
 
-void prepare_computations(t_computations *comps, const t_intersection *intersection, const t_ray *ray) 
+void	prepare_computations(t_computations *comps,
+		const t_intersection *intersection, const t_ray *ray)
 {
-    if (comps == NULL) 
-        return;
-
-    // comps->color = intersection->obj->shape->material->color;
-    comps->t = intersection->t;
-    comps->object = intersection->obj;
-    comps->point = t_point_position(ray, intersection->t);
-    comps->eyev = vector_negate(ray->direction);
-    comps->normalv = normal_at_shape(intersection->obj, comps->point);
-    comps->reflectv = reflect(ray->direction, comps->normalv);
-    
-    if (vector_dot(comps->normalv, comps->eyev) < 0)
-    {
-        comps->inside = true;
-        comps->normalv = vector_negate(comps->normalv);
-    } else
-        comps->inside = false;
-    comps->over_point = tuple_add(comps->point, vector_multiply(comps->normalv, EPSILON));
+	if (comps == NULL)
+		return ;
+	comps->t = intersection->t;
+	comps->object = intersection->obj;
+	comps->point = t_point_position(ray, intersection->t);
+	comps->eyev = vector_negate(ray->direction);
+	comps->normalv = normal_at_shape(intersection->obj, comps->point);
+	comps->reflectv = reflect(ray->direction, comps->normalv);
+	if (vector_dot(comps->normalv, comps->eyev) < 0)
+	{
+		comps->inside = true;
+		comps->normalv = vector_negate(comps->normalv);
+	}
+	else
+		comps->inside = false;
+	comps->over_point = tuple_add(comps->point, vector_multiply(comps->normalv,
+				EPSILON));
 }
 
-int compare_intersections(const void* a, const void* b) 
+int	compare_intersections(const void *a, const void *b)
 {
-    t_intersection* ia = (t_intersection*)a;
-    t_intersection* ib = (t_intersection*)b;
-    if (ia->t < ib->t) 
-        return -1;
-    else if (ia->t > ib->t) 
-        return 1;
-    else return 0;
+	t_intersection	*ia;
+	t_intersection	*ib;
+
+	ia = (t_intersection *)a;
+	ib = (t_intersection *)b;
+	if (ia->t < ib->t)
+		return (-1);
+	else if (ia->t > ib->t)
+		return (1);
+	else
+		return (0);
 }
+
+bool	realloc_intersections(t_intersection **intersections,
+		int required_capacity, int current_capacity)
+{
+	t_intersection	*temp;
+
+	while (required_capacity > current_capacity)
+	{
+		current_capacity *= 2;
+	}
+	temp = realloc(*intersections, current_capacity * sizeof(t_intersection));
+	if (!temp)
+	{
+		return (false);
+	}
+	*intersections = temp;
+	return (true);
+}
+
+typedef struct {
+    t_intersection **intersections;
+    t_intersection *local_intersections;
+    int local_count;
+    t_object *object;
+    int *count;
+} IntersectionAddParams;
+
+
+void add_local_inter(IntersectionAddParams *params) {
+    for (int j = 0; j < params->local_count; j++) {
+        (*(params->intersections))[*(params->count) + j] = params->local_intersections[j];
+        (*(params->intersections))[*(params->count) + j].obj = params->object;
+    }
+    *(params->count) += params->local_count;
+}
+
 
 /**
  * Computes all intersections of a given ray with every object in the world.
  * Allocates and returns a list of sort intersections,
  */
 
-t_intersection* intersect_world(t_world* world, t_ray* ray, int* count) {
-    *count = 0;
-    int capacity = 10;
-    t_intersection* intersections = malloc(capacity * sizeof(t_intersection));
-    if (!intersections) {
-        return NULL;
-    }
-    for (int i = 0; i < world->object_count; i++) {
-        int local_count = 0;
-        t_intersection* local_intersections = intersect_shape(world->objects[i], ray, &local_count);
-        if (local_count > 0) {
-            if (*count + local_count > capacity) {
-                while (*count + local_count > capacity) {
-                    capacity *= 2;
-                }
-                t_intersection* temp = realloc(intersections, capacity * sizeof(t_intersection));
-                if (!temp) {
-                    free(intersections);
-                    free(local_intersections);
-                    return NULL;
-                }
-                intersections = temp;
-            }
-            ft_memcpy(intersections + *count, local_intersections, local_count * sizeof(t_intersection));
-            for (int j = 0; j < local_count; j++) {
-                intersections[*count + j].obj = world->objects[i];
-            }
-            *count += local_count;
-        }
-        free(local_intersections);
-    }
+t_intersection	*intersect_world(t_world *world, t_ray *ray, int *count)
+{
+	int				capacity;
+	t_intersection	*intersections;
+	int				local_count;
+	t_intersection	*local_inter;
+	int				i;
 
-    qsort(intersections, *count, sizeof(t_intersection), compare_intersections);
-    return intersections;
+	i = 0;
+	capacity = 10;
+	intersections = malloc(capacity * sizeof(t_intersection));
+	*count = 0;
+	while (i < world->object_count)
+	{
+		local_count = 0;
+		local_inter = intersect_shape(world->objects[i], ray, &local_count);
+		if (local_count > 0)
+		{
+			if (*count + local_count > capacity)
+			{
+				if (!realloc_intersections(&intersections, *count + local_count,
+						capacity))
+				{
+					free(intersections);
+					free(local_inter);
+					return (NULL);
+				}
+			}
+			add_local_inter(&intersections, local_inter, local_count,
+				world->objects[i], count);
+		}
+		free(local_inter);
+		i++;
+	}
+	qsort(intersections, *count, sizeof(t_intersection), compare_intersections);
+	return (intersections);
 }
-
-
--- world/render.c
+++ world/render.c
@@ -2,56 +2,67 @@
 #include "../include/mathematique.h"
 #include "../include/shape.h"
 #include <math.h>
-#include <stdio.h>
 #include <pthread.h>
+#include <stdio.h>
 
-typedef struct 
+typedef struct s_renderthread
 {
-    t_mlx *mlx;
-    t_world *world;
-    t_camera *camera;
-    t_matrix inverse_transform;
-    int start_y;
-    int end_y;
-} RenderThread;
+	t_mlx		*mlx;
+	t_world		*world;
+	t_camera	*camera;
+	t_matrix	inverse_transform;
+	int			start_y;
+	int			end_y;
+}				t_renderthread;
 
 // render fonction avec remaining pour nombre de reflect a calculer
-void* render_section(void* arg) 
+void	*render_section(void *arg)
 {
-    RenderThread* data = (RenderThread*) arg;
-    int remaining = 2;
-    int x;
-    int y;
-    for (y = data->start_y; y < data->end_y; y++) {
-        for (x = 0; x < data->world->camera->hsize; x++) {
-            t_ray ray = ray_for_pixel(data->world->camera, &data->inverse_transform, x, y);
-            t_color color = color_at(data->world, &ray, remaining);
-            draw_pixel(data->mlx, x, y, color);
-        }
-    }
-    return NULL;
+	t_renderthread	*data;
+	int				remaining;
+	int				x;
+	int				y;
+	t_ray			ray;
+	t_color			color;
+
+	data = (t_renderthread *)arg;
+	remaining = 2;
+	for (y = data->start_y; y < data->end_y; y++)
+	{
+		for (x = 0; x < data->world->camera->hsize; x++)
+		{
+			ray = ray_for_pixel(data->world->camera, &data->inverse_transform,
+					x, y);
+			color = color_at(data->world, &ray, remaining);
+			draw_pixel(data->mlx, x, y, color);
+		}
+	}
+	return (NULL);
 }
 
-void render(t_world *world) 
+void	render(t_world *world)
 {
-    const int num_threads = 15;
-    pthread_t threads[num_threads];
-    RenderThread thread_data[num_threads];
-    t_matrix inverse_transform = matrix_inverse(world->camera->transform);
-    int segment_height = world->camera->vsize / num_threads;
-    int i;
+	const int		num_threads = 15;
+	pthread_t		threads[num_threads];
+	t_renderthread	thread_data[num_threads];
+	t_matrix		inverse_transform;
+	int				segment_height;
+	int				i;
 
-    for (i = 0; i < num_threads; i++) {
-        thread_data[i].mlx = world->mlx;
-        thread_data[i].world = world;
-        thread_data[i].camera = world->camera;
-        thread_data[i].inverse_transform = inverse_transform;
-        thread_data[i].start_y = i * segment_height;
-        thread_data[i].end_y = (i == num_threads - 1) ? world->camera->vsize : (i + 1) * segment_height;
-
-        pthread_create(&threads[i], NULL, render_section, &thread_data[i]);
-    }
-    for (i = 0; i < num_threads; i++)
-        pthread_join(threads[i], NULL);
-    draw_render_to_img(world, world->mlx);
+	inverse_transform = matrix_inverse(world->camera->transform);
+	segment_height = world->camera->vsize / num_threads;
+	for (i = 0; i < num_threads; i++)
+	{
+		thread_data[i].mlx = world->mlx;
+		thread_data[i].world = world;
+		thread_data[i].camera = world->camera;
+		thread_data[i].inverse_transform = inverse_transform;
+		thread_data[i].start_y = i * segment_height;
+		thread_data[i].end_y = (i == num_threads
+				- 1) ? world->camera->vsize : (i + 1) * segment_height;
+		pthread_create(&threads[i], NULL, render_section, &thread_data[i]);
+	}
+	for (i = 0; i < num_threads; i++)
+		pthread_join(threads[i], NULL);
+	draw_render_to_img(world, world->mlx);
 }
