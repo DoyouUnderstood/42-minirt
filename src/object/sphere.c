d6c088a1e3822618d50b918574e67f059c453831
Eric Rabbath <erabbath@gmail.com>
2024-05-23 16:17:53 +0200

    Cleaned sphere constructor (and corrected local intersect calculation)

--- src/object/sphere.c
+++ src/object/sphere.c
@@ -6,7 +6,7 @@
 /*   By: erabbath <erabbath@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 17:53:22 by erabbath          #+#    #+#             */
-/*   Updated: 2024/05/23 15:32:33 by erabbath         ###   ########.fr       */
+/*   Updated: 2024/05/23 16:17:00 by erabbath         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,6 @@
 #include <float.h>
 #include <math.h>
 #include <stdlib.h>
-
-static t_sphere_data	*sphere_create_data(t_sphere_data *data)
-{
-	t_sphere_data	*sphere_data;
-
-	sphere_data = malloc(sizeof(t_sphere_data));
-	if (!sphere_data)
-		return (NULL);
-	*sphere_data = *data;
-	return (sphere_data);
-}
 
 static t_intersection	*create_intersections(t_object *obj, double t1, double t2, double discriminant,
 		int *out_count)
@@ -54,11 +43,11 @@ static t_intersection	*create_intersections(t_object *obj, double t1, double t2,
 static t_intersection	*sphere_intersect(t_object *obj, t_ray *ray,
 		int *out_count)
 {
-	t_sphere_data			*data;
 	t_sphere_intersect_calc	calc;
+	static t_tuple			world_origin = {0, 0, 0, 0};
 
-	data = (t_sphere_data *) obj->data;
-	calc.sphere_to_ray = tuple_subtract(ray->origin, data->center);
+	(void) obj;
+	calc.sphere_to_ray = tuple_subtract(ray->origin, world_origin);
 	calc.a = vector_dot(ray->direction, ray->direction);
 	calc.b = 2 * vector_dot(ray->direction, calc.sphere_to_ray);
 	calc.c = vector_dot(calc.sphere_to_ray, calc.sphere_to_ray) - 1.0;
@@ -88,13 +77,12 @@ static t_tuple	sphere_normal_at(t_object *obj, t_tuple local_point)
 t_object	*sphere_create(t_sphere_data *data, t_color color,
 		double reflectivity, t_pattern *pattern)
 {
-	t_sphere_data	*sphere_data;
 	t_object		*obj;
 
-	sphere_data = sphere_create_data(data);
 	obj = malloc(sizeof(t_object));
+	obj->data = malloc(sizeof(t_sphere_data));
+	*((t_sphere_data *) obj->data) = *data;
 	obj->type = SPHERE;
-	obj->data = sphere_data;
 	obj->transformation = matrix_mult(matrix_translation(data->center.x,
 				data->center.y, data->center.z), matrix_scaling(data->radius, data->radius, data->radius));
 	obj->inv_transformation = matrix_inverse(obj->transformation);
@@ -103,6 +91,5 @@ t_object	*sphere_create(t_sphere_data *data, t_color color,
 			reflectivity, pattern);
 	obj->local_intersect = sphere_intersect;
 	obj->local_normal_at = sphere_normal_at;
-	sphere_data->center = (t_tuple){0, 0, 0, 0};
 	return (obj);
 }
