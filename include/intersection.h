d4b1a9b8bb743be233ddbd60051b230c4d667c6f
Eric Rabbath <erabbath@gmail.com>
2024-05-25 21:49:59 +0200

    Removed unused functions from interactions

--- include/intersection.h
+++ include/intersection.h
@@ -6,7 +6,7 @@
 /*   By: erabbath <erabbath@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 16:26:50 by erabbath          #+#    #+#             */
-/*   Updated: 2024/05/25 20:37:03 by erabbath         ###   ########.fr       */
+/*   Updated: 2024/05/25 21:48:50 by erabbath         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,6 @@ typedef struct s_intersection_pair
 	t_object	*obj;
 }	t_intersection_pair;
 
-t_intersection	*intersection_create(t_object *obj, double t);
-t_intersection	*intersection_create_pair(t_object *obj, double t0, double t1);
 bool			intersection_realloc(t_intersection **intersections,
 					int required_capacity, int *capacity);
 t_intersection	*intersection_hit(t_intersection *intersections, int count);
--- src/intersection.c
+++ src/intersection.c
@@ -6,7 +6,7 @@
 /*   By: erabbath <erabbath@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 16:26:50 by erabbath          #+#    #+#             */
-/*   Updated: 2024/05/25 11:52:47 by erabbath         ###   ########.fr       */
+/*   Updated: 2024/05/25 21:48:40 by erabbath         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,32 +15,6 @@
 
 #include <float.h>
 #include <stdlib.h>
-
-t_intersection	*intersection_create(t_object *obj, double t)
-{
-	t_intersection	*intersection;
-
-	intersection = malloc(sizeof(*intersection));
-	if (!intersection)
-		return (NULL);
-	intersection->obj = obj;
-	intersection->t = t;
-	return (intersection);
-}
-
-t_intersection	*intersection_create_pair(t_object *obj, double t0, double t1)
-{
-	t_intersection	*intersections;
-
-	intersections = malloc(sizeof(*intersections) * 2);
-	if (!intersections)
-		return (NULL);
-	intersections[0].obj = obj;
-	intersections[0].t = t0;
-	intersections[1].obj = obj;
-	intersections[1].t = t1;
-	return (intersections);
-}
 
 bool	intersection_realloc(t_intersection **intersections,
 		int required_capacity, int *capacity)
